d9a6a2d076d4674fd02497d352c74c5813cb9d22
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2023-03-26 20:49:44 +0200

    menu bar

--- assets/script/script.js
+++ assets/script/script.js
@@ -54,6 +54,12 @@ $(document).ready(function () {
   });
   
 
+  hamburger = document.querySelector(".hamburger");
+  hamburger.onclick = function(){
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active")
+  }
+
 
 
 
